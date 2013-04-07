ea7eca107ca158010733269f95102abccd126631
Jens Nilsson <jens.nilsson@24hr.se>
2013-04-07 11:48:18 +0200

    Save zoom-level and map center

--- js/leaflet-field.js
+++ js/leaflet-field.js
@@ -6,60 +6,67 @@ jQuery(document).ready(function($) {
     }
 
     function render_leaflet_map() {
-        var map_settings = {};
-
-        var value = {
-            markers:[]
-        };
-
         var field = $('#' + leaflet_field.id);
 
-        if( typeof leaflet_field.lat != 'undefined' ) {
-            map_settings.lat = leaflet_field.lat;
-        }
-        else {
-            map_settings.lat = 0;
+        var map_settings = {
+            zoom_level:null,
+            center:{
+                lat:null,
+                lng:null
+            },
+            markers:[],
+        };
+
+        // check if we have a saved value
+        if( field.val().length > 0 ) {
+            map_settings = JSON.parse(field.val());
         }
 
-        if( typeof leaflet_field.lng != 'undefined' ) {
-            map_settings.lng = leaflet_field.lng;
+        if( typeof map_settings.center.lat == 'undefined' ) {
+            map_settings.center.lat = leaflet_field.lat;
         }
-        else {
-            map_settings.lng = 0;
+
+        if( typeof map_settings.center.lng == 'undefined' ) {
+            map_settings.center.lng = leaflet_field.lng;
         }
 
         // check if the zoom level is set and within 1-18
-        if( typeof leaflet_field.zoom_level != 'undefined' || leaflet_field.zoom_level > 18 || leaflet_field.zoom_level < 1 ) {
-            map_settings.zoom_level = leaflet_field.zoom_level;
+        if( typeof map_settings.zoom_level == 'undefined' || map_settings.zoom_level > 18 || map_settings.zoom_level < 1 ) {
+            if( leaflet_field.zoom_level > 0 && leaflet_field.zoom_level < 19 ) {
+                map_settings.zoom_level = leaflet_field.zoom_level;
+            }
+            else {
+                map_settings.zoom_level = 13;
+            }
         }
-        else {
-            map_settings.zoom_level = 13;
-        }
 
-
-        console.log(map_settings);
-
-        var map = L.map('map').setView( [map_settings.lat, map_settings.lng], map_settings.zoom_level );
+        var map = L.map('map').setView( [map_settings.center.lat, map_settings.center.lng], map_settings.zoom_level );
 
         L.tileLayer('http://{s}.tile.cloudmade.com/' + leaflet_field.api_key + '/997/256/{z}/{x}/{y}.png', {
             attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
             maxZoom: 18
         }).addTo(map);
 
-        if( field.val.length > 0 ) {
-            var test = JSON.parse(field.val());
-            
-            $.each(test.markers, function(index, marker){
+
+        // render existing markers
+        if( map_settings.markers.length > 0 ) {
+            $.each(map_settings.markers, function(index, marker){
                 L.marker(marker, {draggable: true}).addTo(map);
-                value.markers.push(marker);
             });
         }
 
         map.on('click', function(e){
             var marker = L.marker(e.latlng, {draggable: true}).addTo(map);
-            value.markers.push(e.latlng);
-
-            field.val(JSON.stringify(value));
+            map_settings.markers.push(e.latlng);
+            field.val(JSON.stringify(map_settings));
+        }).on('zoomend', function(e){
+            map_settings.zoom_level = map.getZoom();
+            field.val(JSON.stringify(map_settings));
+        }).on('dragend', function(e){
+            var center = map.getCenter();
+            map_settings.center.lat = center.lat;
+            map_settings.center.lng = center.lng;
+            field.val(JSON.stringify(map_settings));
         });
     }
 
